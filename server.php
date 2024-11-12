fe1a417a170d442120bd2191e83cc198b04f4bb0
ReallyNewb <bradenlebron@icloud.com>
2024-11-11 20:36:09 -0500

    Im just testing api stuff i think

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <body>
-  if ($_SERVER["validation"] !== "validate me pls") {
+  if ($_SERVER["validation"] == "validate me pls") {
     echo '1:$_POST["username"]:2:$_POST["pwd"]:3:$_POST["email"]'
   }
 </body>
