ab644f3268f1f9a5084f624d3071f6febb110f9d
ReallyNewb <bradenlebron@icloud.com>
2024-11-11 20:46:03 -0500

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($_SERVER["validation"] == "validate me pls") {
-    echo '1:$_POST["username"]:2:$_POST["pwd"]:3:$_POST["email"]'
+    return '1:$_POST["username"]:2:$_POST["pwd"]:3:$_POST["email"]'
   }
 }
