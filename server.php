e7e54ae493f69f79da082a485637d5c8c3e5478f
ReallyNewb <bradenlebron@icloud.com>
2024-11-11 20:55:48 -0500

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,11 @@
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
   if ($_SERVER["validation"] == "validate me pls") {
-    return '1:$_POST["username"]:2:$_POST["pwd"]:3:$_POST["email"]'
+    $user = htmlspecialchars($_GET["username"])
+    $pwd = htmlspecialchars($_GET["pwd"])
+    $mail = htmlspecialchars($_GET["email"])
+
+    return "1:$user:2:$pwd:3:$mail"
+  } else {
+    return '-1'
   }
 }
